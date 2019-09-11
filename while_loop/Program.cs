d045a93a670306806a0bd5ebfa796e64856d27e9
daniel <naguri@mail.usf.edu>
2019-09-10 20:53:12 -0400

    Add project files.

---
+++ while_loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter Experience");
+                string years = Console.ReadLine();
+                int year_of_exp = Int32.Parse(years);
+                while(year_of_exp > 0)
+                {
+                    
+                    Console.WriteLine("You have " + year_of_exp.ToString() + " years of Experience");
+                    year_of_exp = year_of_exp - 1;
+                }
+
+            }
+            catch(InvalidCastException)
+            {
+                Console.WriteLine("Enter Valid Years of Experience");
+            }
+        }
+    }
+}
